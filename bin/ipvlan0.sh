5c85bebb73473073b4ffe277ed7efaaa55f57f52
estuardoquan <estuardo.quan@gmail.com>
2025-11-17 15:47:18 -0600

    do not know

--- bin/ipvlan0.sh
+++ bin/ipvlan0.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ip link add ipvlan0 link [INTERFACE] type ipvlan mode l2
-ip addr add [IP/SUBNET] dev ipvlan0
-ip link set ipvlan0 up
+DEV=${1:-ipvlan0}
+INT=${2:?'Error: Please provide interface'}
+ADDR=${3:?'Please provide address'}
+
+ip link add ${DEV} link ${INT} type ipvlan mode l2
+ip addr add ${ADDR} dev ${DEV}
+ip link set ${DEV} up
