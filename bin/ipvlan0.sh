b11c748b773c037cca1ba6e165858a56990ca309
estuardoquan <estuardo.quan@gmail.com>
2025-11-17 16:35:08 -0600

    made new shit

--- bin/ipvlan0.sh
+++ bin/ipvlan0.sh
@@ -1,8 +1,45 @@
 #!/bin/sh
 
-DEV=${1:-ipvlan0}
-INT=${2:?'Error: Please provide interface'}
-ADDR=${3:?'Please provide address'}
+ADDR=192.68.1.64/24
+DEV=ipvlan0
+INT=enp2s0
+
+OPTIONS="a:d:i:"
+LONGOPTIONS="addr:,dev:,int:"
+
+GETOPT=$(getopt -o "${OPTIONS}" --long "${LONGOPTIONS}" -n "$(basename "$0")" -- "${@}")
+
+if [ $? -ne 0 ]; then
+        echo "Error parsing options."
+        exit 1
+fi
+
+eval set -- "${GETOPT}"
+
+while true; do
+        case "$1" in
+        -a | --addr)
+                ADDR="$2"
+                shift 2
+                ;;
+        -d | --dev)
+                DEV="$2"
+                shift 2
+                ;;
+        -i | --int)
+                INT="$2"
+                shift 2
+                ;;
+        --)
+                shift
+                break
+                ;;
+        *)
+                echo "Internal error!"
+                exit 1
+                ;;
+        esac
+done
 
 ip link add ${DEV} link ${INT} type ipvlan mode l2
 ip addr add ${ADDR} dev ${DEV}
